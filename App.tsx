17bd35b928a43d5a5f6143a93aaff4336615e46b
tigredonorte <tigredonorte3@gmail.com>
2022-03-09 11:04:21 -0300

    receiving notifications by server

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
-import React, { useEffect } from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, Platform, StyleSheet, View } from 'react-native';
 import { Button } from 'react-native-paper';
 import * as notifications from 'expo-notifications';
+import * as taskManager from 'expo-task-manager';
+
+const BACKGROUND_NOTIFICATION_TASK = 'BACKGROUND-NOTIFICATION-TASK';
+taskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, ({ data, error, executionInfo }) => {
+  console.log('Received a notification in the background!');
+  // Do something with the notification data
+});
+notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
 
 notifications.setNotificationHandler({
   handleNotification: async(data) => ({
@@ -13,14 +21,16 @@ notifications.setNotificationHandler({
 
 export default function App() {
 
-  useEffect(() => {
+  const [ token, setToken ] = useState('');
 
+  // notification listeners
+  useEffect(() => {
     const backgroundSubs = notifications.addNotificationResponseReceivedListener((notification) => {
-      console.log({ notification }, '================');
+      console.log({ notification }, 'backgroundSubs');
     });
 
     const foregroundSubs = notifications.addNotificationReceivedListener((notification) => {
-      console.log({ notification });
+      console.log({ notification }, 'foregroundSubs');
     });
 
     return () => {
@@ -29,13 +39,39 @@ export default function App() {
     }
   }, []);
 
-  const notify = async() => {
-    const perm = await notifications.requestPermissionsAsync();
-    if (!perm.granted) {
-      return Alert.alert('Permission denied', "You must grant notification permission to use this app");
+  const askForPermission = async() => {
+    if (Platform.OS === 'android') {
+      return true;
     }
+    let perm = await notifications.getPermissionsAsync();
+    if (!perm.granted) {
+      if (!perm.canAskAgain) {
+        return false;
+      }
 
-    const res = await notifications.scheduleNotificationAsync({
+      perm = await notifications.requestPermissionsAsync();
+      if (!perm.granted) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // request permission
+  useEffect(() => {
+    const init = async() => {
+      if (!await askForPermission()) {
+        return Alert.alert('Permission denied', "You must grant notification permission to use this app");
+      }
+      
+      const response = await notifications.getExpoPushTokenAsync();
+      setToken(response.data); // here you must save the user's token.
+    };
+    init();
+  }, [ setToken ]);
+
+  const scheduleLocalNotification = async() => {
+    await notifications.scheduleNotificationAsync({
       content: {
         title: 'My first notification',
         body: 'Can i read long notifications? I dont know, but writing this big notification I can test it! ',
@@ -44,15 +80,37 @@ export default function App() {
         }
       },
       trigger: {
-        seconds: 2
+        seconds: 5
       }
     });
-    console.log(res);
+  }
+
+  const scheduleRemoteNotification = async() => {
+    try {
+      await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Accept-Encoding': 'gzip, deflate',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          to: token,
+          data: { foo: 'bar' },
+          title: 'Sent via app',
+          body: 'this push was sent via the api'
+        })
+      });
+  
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 
   return (
     <View style={styles.container}>
-      <Button onPress={notify}>Trigger notification</Button>
+      <Button onPress={scheduleLocalNotification}>Local notification</Button>
+      <Button mode='contained' onPress={scheduleRemoteNotification}>Remote notification</Button>
     </View>
   );
 }
--- src/app/types/env.d.ts
+++ src/app/types/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
     export const PRODUCTION: string;
     export const LOG_ENVIRONMENT: string;
+    export const GOOGLE_NOTIFICATION_KEY: string;
 }
