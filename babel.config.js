c75a4d391a0fa4cd281495db6d1a3b558732b787
tigredonorte <tigredonorte3@gmail.com>
2022-03-09 06:30:58 -0300

    initial setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [ 'babel-preset-expo' ],
+    plugins: [
+      [
+        'module-resolver', {
+          root: [ './' ],
+          alias: {
+            '^~(.+)': './src/\\1', // All paths starting with ~ will be replaced with ./src/
+          }
+        }
+      ],
+      [
+        'module:react-native-dotenv', {
+          'moduleName': '@env',
+          'path': '.env',
+          'blocklist': null,
+          'allowlist': null,
+          'safe': true,
+          'allowUndefined': true
+        }
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
---
+++ src/app/types/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+    export const PRODUCTION: string;
+    export const LOG_ENVIRONMENT: string;
+}
