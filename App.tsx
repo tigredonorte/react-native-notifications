cb1a22d6bc597904a64a41550adfef0861dee154
tigredonorte <tigredonorte3@gmail.com>
2022-03-09 07:01:58 -0300

    simple in-app notification

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import * as notifications from 'expo-notifications';
 
 export default function App() {
+
+  const notify = async() => {
+    const perm = await notifications.requestPermissionsAsync();
+    if (!perm.granted) {
+      return Alert.alert('Permission denied', "You must grant notification permission to use this app");
+    }
+
+    const res = await notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first notification',
+        body: 'Can i read long notifications? I dont know, but writing this big notification I can test it! '
+      },
+      trigger: {
+        seconds: 10
+      }
+    });
+    console.log(res);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={notify}>Trigger notification</Button>
     </View>
   );
 }
